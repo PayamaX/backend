8ba93cb34a21fa45787c7c2f9f12bf59663c585a
Homayoun <eqpc@hbn.dev>
2025-03-11 10:45:16 +0100

    tests adding...

---
+++ api.Tests/Controllers/PayamaxPublicControllerUploadPayamaksTest.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using PayamaX.Portal.Controllers;
+using Xunit;
+
+namespace api.Tests.Controllers;
+
+[TestSubject(typeof(PayamaxPublicController))]
+public class PayamaxPublicControllerUploadPayamaksTest
+{
+
+    [Fact]
+    public void whenCallingUploadPayamaks_thenShouldPersistPayamaks()
+    {
+        
+    }
+}
